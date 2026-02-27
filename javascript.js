08d70143bb81d8d3678bc562b466b9fd7aebcddd
Vboxuser <127411353+StepByStep2002@users.noreply.github.com>
2026-02-27 15:15:30 +0200

    Changed win condition to reaching 5 points

--- javascript.js
+++ javascript.js
@@ -8,59 +8,47 @@ function playRound(humanChoice, computerChoice){
         case "rock":
         if(computerChoice === "scissors")
         {
-            numberOfRounds++;
             humanScore++;
-            console.log(numberOfRounds)
            return result.textContent = `You win! Rock beats Scissors!\n Your score: ${humanScore}\n Computer's score: ${computerScore}`;
         }
         else if (computerChoice === "paper")
         {
-            numberOfRounds++;
             computerScore++;
-            console.log(numberOfRounds)
             return result.textContent = `You lose! Paper beats Rock!\n Your score: ${humanScore}\n Computer's score: ${computerScore}`;
         }
         else
         {
-            numberOfRounds++;
-            console.log(numberOfRounds)
             return result.textContent = `It's a tie!\n Your score: ${humanScore}\n Computer's score: ${computerScore}`;
         }
 
         case "paper":
             if(computerChoice === "rock")
             {
-            numberOfRounds++;
             humanScore++;
             return result.textContent = `You win! Paper beats Rock!\n Your score: ${humanScore}\n Computer's score: ${computerScore}`;
             }
         else if (computerChoice === "scissors")
         {
-            numberOfRounds++;
             computerScore++;
             return result.textContent = `You lose! Scissors beat Paper\n Your score: ${humanScore}\n Computer's score: ${computerScore}`;
         }
         else
         {
-            numberOfRounds++;
             return result.textContent = `It's a tie!\n Your score: ${humanScore}\n Computer's score: ${computerScore} `;
         }
 
         case "scissors":
         if(computerChoice === "paper")
         {
-            numberOfRounds++;
             humanScore++;
             return result.textContent = `You win! Scissors beat Paper\n Your score: ${humanScore}\n Computer's score: ${computerScore}`;
         }
         else if (computerChoice === "rock")
         {
-            numberOfRounds++;
             computerScore++;
             return result.textContent = `You lose! Rock beats Scissors\n Your score: ${humanScore}\n Computer's score: ${computerScore}`;
         }
         else
-            numberOfRounds++;
             return result.textContent =`It's a tie!\n Your score: ${humanScore}\n Computer's score: ${computerScore} `;
 
         }
@@ -91,29 +79,24 @@ function getComputerChoice(){
 
 function gameEnd()
 {
-    if(numberOfRounds === 5)
+    if(humanScore == 5)
     {
         choiceRock.disabled = true;
         choicePaper.disabled = true;
         choiceScissors.disabled = true;
-    
-        if(humanScore > computerScore)
-        {
-            finalResult.textContent = `You win!\nYour score: ${humanScore}\nComputer score: ${computerScore}`
-        }
-        else if(humanScore < computerScore)
-        {
-            finalResult.textContent = `You lose!\nYour score: ${humanScore}\nComputer score: ${computerScore}`
-        }
-        else
-        {
-            finalResult.textContent = `Game ended in a tie!\nYour score: ${humanScore}\nComputer score: ${computerScore}`
-        }
-}
+        finalResult.textContent = `You win!\n Your score: ${humanScore}\n Computer score: ${computerScore}`
+    }
+    else if(computerScore == 5)
+    {
+        choiceRock.disabled = true;
+        choicePaper.disabled = true;
+        choiceScissors.disabled = true;
+        finalResult.textContent = `You lose!\n Your score: ${humanScore}\n Computer score: ${computerScore}`
+    }
+
 }
 let humanScore = 0
 let computerScore = 0
-let numberOfRounds = 0
 let result = document.getElementById("results")
 let finalResult = document.getElementById("finalResults")
 const choiceRock = document.getElementById("rock");
